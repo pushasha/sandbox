c44a66ac3c3d8fb2db00c6016773a604d49a378d
Sarah Plotkin <sarah.plotkin@apx-labs.com>
2016-02-03 17:24:09 -0500

    Initial commit.

---
+++ bootstrap.bat
@@ -0,0 +1,24 @@
+@echo off
+cls
+
+rem Save off the script directory
+set script_dir=%~dp0
+
+rem Pop the current working directory so we can return later
+pushd %cd%
+
+rem For paket to work off the script directory, we have to change the working directory
+cd %script_dir%
+
+.paket\paket.bootstrapper.exe
+if errorlevel 1 (
+    exit /b %errorlevel%
+)
+
+.paket\paket.exe restore
+if errorlevel 1 (
+    exit /b %errorlevel%
+)
+
+rem Return to the directory we originally called this from
+popd
