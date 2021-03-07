0557ee092e14c59996e004e4156f985309064913
Sarah Plotkin <pushasha@gmail.com>
2021-03-07 16:17:21 -0500

    Adding common types header for typedefs.

---
+++ cpp_data_structures/CommonTypes.h
@@ -0,0 +1,10 @@
+#ifndef COMMONTYPES_H
+#define COMMONTYPES_H
+
+typedef char byte;
+typedef unsigned char ubyte;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
+#endif //COMMONTYPES_H
